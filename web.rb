3e0b7bac12796afaf26de89de7111d6fecba9b40
Jake Boxer <jake@github.com>
2014-04-28 16:29:33 -0700

    Clean up variables

--- web.rb
+++ web.rb
@@ -3,11 +3,10 @@ require 'ruby_cowsay'
 require 'sinatra'
 
 get '/' do
-  msg = params[:msg]
+  msg = params[:msg] || "usage: GET /?msg=sup"
   cow_type = params[:cow] || 'default'
   face_type = params[:face] || 'default'
 
   content_type 'text/plain'
-  msg ||= "usage: GET /?msg=sup"
   Cow.say(msg, cow_type, face_type)
 end
