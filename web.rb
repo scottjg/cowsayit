930766b26a9e23fbd78c704b32a038668e6f3068
Jake Boxer <jake@github.com>
2014-04-28 16:30:21 -0700

    Whitespace

--- web.rb
+++ web.rb
@@ -3,8 +3,8 @@ require 'ruby_cowsay'
 require 'sinatra'
 
 get '/' do
-  msg = params[:msg] || "usage: GET /?msg=sup"
-  cow_type = params[:cow] || 'default'
+  msg       = params[:msg] || "usage: GET /?msg=sup"
+  cow_type  = params[:cow] || 'default'
   face_type = params[:face] || 'default'
 
   content_type 'text/plain'
