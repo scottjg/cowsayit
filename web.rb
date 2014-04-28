f5570ff895436d08c0b94602d043fb131a1d4279
Jake Boxer <jake@github.com>
2014-04-28 16:30:37 -0700

    Quote consistency

--- web.rb
+++ web.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
-require 'ruby_cowsay'
-require 'sinatra'
+require "ruby_cowsay"
+require "sinatra"
 
-get '/' do
+get "/" do
   msg       = params[:msg] || "usage: GET /?msg=sup"
-  cow_type  = params[:cow] || 'default'
-  face_type = params[:face] || 'default'
+  cow_type  = params[:cow] || "default"
+  face_type = params[:face] || "default"
 
-  content_type 'text/plain'
+  content_type "text/plain"
   Cow.say(msg, cow_type, face_type)
 end
