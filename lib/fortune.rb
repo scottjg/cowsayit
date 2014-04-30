e2ea2691d187c85faa7889d6627b0d9963c9558f
Scott J. Goldman <scottjg@github.com>
2014-04-29 23:29:34 -0700

    add fortune endpoint

---
+++ lib/fortune.rb
@@ -0,0 +1,23 @@
+class Fortune
+  def fortune_exe
+    return @fortune_exe if @fortune_exe
+
+    # for heroku
+    @fortune_exe = ".apt/usr/games/fortune -s .apt/usr/share/games/fortunes/"
+    begin
+      `#{@fortune_exe}`
+      return @fortune_exe
+    rescue Errno::ENOENT
+    end
+
+    # everywhere else
+    @fortune_exe = "fortune -s"
+    `#{@fortune_exe}`
+
+    @fortune_exe
+  end
+
+  def get
+    `#{fortune_exe}`.gsub(/\t/, "  ").rstrip
+  end
+end
--- web.rb
+++ web.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require "ruby_cowsay"
+require "fortune"
 require "sinatra"
 
 get "/" do
@@ -11,10 +12,21 @@ get "/" do
     "\n" +
     "cows: #{Cow.cows.join(" ")}\n" +
     "\n" +
-    "faces: #{Cow.faces.join(" ")}"
+    "faces: #{Cow.faces.join(" ")}" +
+
   cow_type  = params[:cow] || "default"
   face_type = params[:face] || "default"
 
+  content_type "text/plain"
+  Cow.say(msg, cow_type, face_type)
+end
+
+get "/fortune" do
+  msg       = Fortune.new.get
+  cow_type  = params[:cow] || "default"
+  face_type = params[:face] || "default"
+
+  p msg
   content_type "text/plain"
   Cow.say(msg, cow_type, face_type)
 end
